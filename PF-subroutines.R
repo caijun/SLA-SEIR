e2856926785fd3374c0155835027e67948092068
caijun <tony.tsai.2046@gmail.com>
2016-04-01 00:04:26 +0800

    optimize PF1 function

--- PF-subroutines.R
+++ PF-subroutines.R
@@ -20,14 +20,14 @@ PF <- function(y, yI, a0, b0, c0, d0, ma, sda, mb, sdb, mg, sdg, M, a) {
   R_t <- rep(0, M)
   b0_t <- rep(b0, M) # parameter learning for \sigma_g^2
   d0_t <- rep(d0, M) # parameter learning for \sigma_y^2
-  qs <- array(0, c(10, 3, n)) # quantiles of 10 parameters of interest
   pars <- cbind(alpha, beta, gamma)
   lpars <- log(pars)
+  qs <- array(0, c(10, 3, n)) # quantiles of 10 parameters of interest
   for (t in 1:n) {
     print(t)
-    mp <- apply(lpars, 2, mean)
-    vp <- var(lpars)
-    lpars <- a * lpars + (1 - a) * matrix(mp, M, 3, byrow = TRUE) # a is eta
+    m_psi <- apply(lpars, 2, mean)
+    V_psi <- var(lpars)
+    lpars <- a * lpars + (1 - a) * matrix(m_psi, M, 3, byrow = TRUE) # a is eta
     pars <- exp(lpars)
     # Resampling
     mu_g <- -pars[, 3] + pars[, 1] * E_t / I_t
@@ -45,7 +45,7 @@ PF <- function(y, yI, a0, b0, c0, d0, ma, sda, mb, sdb, mg, sdg, M, a) {
     b0_t0 <- b0_t[k]
     d0_t0 <- d0_t[k]
     # Propagation
-    lpars <- lpars + matrix(rnorm(M * 3), M, 3) %*% chol(h2 * vp)
+    lpars <- lpars + matrix(rnorm(M * 3), M, 3) %*% chol(h2 * V_psi)
     pars <- exp(lpars)
     mu_g <- -pars[, 3] + pars[, 1] * E_t0 / I_t0
     B <- 1 / (1 / sig_g2 + 1 / sig_y2)
@@ -112,12 +112,12 @@ PFlike <- function(y, yI, alpha, beta, gamma, a0, b0, c0, d0, M) {
     loglike[t] <- log(mean(exp(w) / y[t])) 
     w1 <- exp(w - max(w))
     k <- sample(1:M, size = M, replace = TRUE, prob = w1)
+    sig_g2 <- sig_g2[k]
+    sig_y2 <- sig_y2[k]
     S_t0 <- S_t[k]
     E_t0 <- E_t[k]
     I_t0 <- I_t[k]
     R_t0 <- R_t[k]
-    sig_g2 <- sig_g2[k]
-    sig_y2 <- sig_y2[k]
     b0_t0 <- b0_t[k]
     d0_t0 <- d0_t[k]
     # Update I
@@ -146,17 +146,17 @@ PF1 <- function(y, yI, a0, b0, c0, d0, ma, sda, mb, sdb, mg, sdg, M, a) {
   # a is the Liu-West shrinage factor
   # h is a controlling smoothing parameter
   h2 <- 1 - a ^ 2
-  sig2 <- 1 / rgamma(M, a0, b0) # evolution variance, \sigma_g^2
-  tau2 <- 1 / rgamma(M, c0, d0) # observation variance, \sigma_y^2
+  sig_g2 <- 1 / rgamma(M, a0, b0) # evolution variance, \sigma_g^2
+  sig_y2 <- 1 / rgamma(M, c0, d0) # observation variance, \sigma_y^2
   alpha <- rtnorm(M, ma, sda)
   beta <- rtnorm(M, mb, sdb)
   gamma <- rtnorm(M, mg, sdg)
-  S <- rep(1 - y[1], M)
-  E <- rep(0, M)
-  I <- rep(y[1], M)
-  R <- rep(0, M)
-  ss <- rep(b0, M) # parameter learning for \sigma_g^2
-  vv <- rep(d0, M) # parameter learning for \sigma_y^2
+  S_t <- rep(1 - y[1], M)
+  E_t <- rep(0, M)
+  I_t <- rep(y[1], M)
+  R_t <- rep(0, M)
+  b0_t <- rep(b0, M) # parameter learning for \sigma_g^2
+  d0_t <- rep(d0, M) # parameter learning for \sigma_y^2
   pars <- cbind(alpha, beta, gamma)
   lpars <- log(pars)
   qs <- array(0, c(10, 3, n)) # quantiles of 10 parameters of interest
@@ -164,71 +164,72 @@ PF1 <- function(y, yI, a0, b0, c0, d0, ma, sda, mb, sdb, mg, sdg, M, a) {
   pred <- matrix(0, M, n) # predictions of the infected
   for (t in 1:n) {
     print(t)
-    pred[, t] <- (1 - pars[, 3]) * I + pars[, 1] * E
-    w <- dnorm(yI[t], -pars[, 3] + pars[, 1] * E / I, sqrt(sig2 + tau2), log = TRUE)
+    pred[, t] <- (1 - pars[, 3]) * I_t + pars[, 1] * E_t
+    mu_g <- -pars[, 3] + pars[, 1] * E_t / I_t
+    w <- dnorm(yI[t], mu_g, sqrt(sig_g2 + sig_y2), log = TRUE) # probabilities p are given as log(p)
     loglike[t] <- log(mean(exp(w) / y[t]))
     ### same code from PF function ###
     m_psi <- apply(lpars, 2, mean)
     V_psi <- var(lpars)
-    lpars <- a * lpars + (1 - a) * matrix(m_psi, M, 3, byrow = TRUE)
+    lpars <- a * lpars + (1 - a) * matrix(m_psi, M, 3, byrow = TRUE) # a is eta
     pars <- exp(lpars)
     # Resampling
-    g <- -pars[, 3] + pars[, 1] * E / I
-    w <- dnorm(yI[t], g, sqrt(sig2 + tau2), log = TRUE) # probabilities p are given as log(p)
+    mu_g <- -pars[, 3] + pars[, 1] * E_t / I_t
+    w <- dnorm(yI[t], mu_g, sqrt(sig_g2 + sig_y2), log = TRUE) # probabilities p are given as log(p)
     w1 <- exp(w - max(w))
     k <- sample(1:M, size = M, replace = TRUE, prob = w1)
     w <- w[k]
-    S1 <- S[k]
-    I1 <- I[k]
-    E1 <- E[k]
-    R1 <- R[k]
-    sig2 <- sig2[k]
-    ss1 <- ss[k]
-    tau2 <- tau2[k]
-    vv1 <- vv[k]
     lpars <- lpars[k, ]
+    sig_g2 <- sig_g2[k]
+    sig_y2 <- sig_y2[k]
+    S_t0 <- S_t[k]
+    I_t0 <- I_t[k]
+    E_t0 <- E_t[k]
+    R_t0 <- R_t[k]
+    b0_t0 <- b0_t[k]
+    d0_t0 <- d0_t[k]
     # Propagation
     lpars <- lpars + matrix(rnorm(M * 3), M, 3) %*% chol(h2 * V_psi)
     pars <- exp(lpars)
-    g1 <- -pars[, 3] + pars[, 1] * E1 / I1
-    var <- 1 / (1 / sig2 + 1 / tau2)
-    mean <- var * (yI[t] / sig2 + g1 / tau2)
-    x <- rnorm(M, mean, sqrt(var))
-    I <- I1 * (1 + x)
+    mu_g <- -pars[, 3] + pars[, 1] * E_t0 / I_t0
+    B <- 1 / (1 / sig_g2 + 1 / sig_y2)
+    b <- B * (yI[t] / sig_g2 + mu_g / sig_y2)
+    g_t <- rnorm(M, b, sqrt(B))
+    I_t <- I_t0 * (1 + g_t)
     # Update (E, R, S)
-    E <- pars[, 2] * I1 * S1 + (1 - pars[, 1]) * E1
-    R <- R1 + pars[, 3] * I1
-    S <- 1 - I - R - E
+    E_t <- pars[, 2] * I_t0 * S_t0 + (1 - pars[, 1]) * E_t0
+    R_t <- R_t0 + pars[, 3] * I_t0
+    S_t <- 1 - I_t - R_t - E_t
     # Reweighting
-    w1 <- dnorm(yI[t], g1, sqrt(sig2 + tau2), log = TRUE) - w
+    w1 <- dnorm(yI[t], mu_g, sqrt(sig_g2 + sig_y2), log = TRUE) - w
     w1 <- exp(w1 - max(w1))
     k <- sample(1:M, size = M, replace = TRUE, prob = w1)
-    S <- S[k]
-    E <- E[k]
-    I <- I[k]
-    R <- R[k]
-    sig2 <- sig2[k]
-    ss1 <- ss1[k]
-    tau2 <- tau2[k]
-    vv1 <- vv1[k]
     lpars <- lpars[k, ]
+    sig_g2 <- sig_g2[k]
+    sig_y2 <- sig_y2[k]
+    S_t <- S_t[k]
+    E_t <- E_t[k]
+    I_t <- I_t[k]
+    R_t <- R_t[k]
+    b0_t0 <- b0_t0[k]
+    d0_t0 <- d0_t0[k]
     # Offline sampling fixed parameters
-    ss <- ss1 + (yI[t] - x) ^ 2 / 2
-    sig2 <- 1 / rgamma(M, a0 + t / 2, ss)
-    vv <- vv1 + (x - g1) ^ 2 / 2
-    tau2 <- 1 / rgamma(M, c0 + t / 2, vv)
+    b0_t <- b0_t0 + (yI[t] - g_t) ^ 2 / 2
+    sig_g2 <- 1 / rgamma(M, a0 + t / 2, b0_t)
+    d0_t <- d0_t0 + (g_t - mu_g) ^ 2 / 2
+    sig_y2 <- 1 / rgamma(M, c0 + t / 2, d0_t)
     # Storage
     pars <- exp(lpars)
-    qs[1, , t] <- quantile(S, c(.05, .5, .95))
-    qs[2, , t] <- quantile(I, c(.05, .5, .95))
-    qs[3, , t] <- quantile(E, c(.05, .5, .95))
-    qs[4, , t] <- quantile(R, c(.05, .5, .95))
+    qs[1, , t] <- quantile(S_t, c(.05, .5, .95))
+    qs[2, , t] <- quantile(I_t, c(.05, .5, .95))
+    qs[3, , t] <- quantile(E_t, c(.05, .5, .95))
+    qs[4, , t] <- quantile(R_t, c(.05, .5, .95))
     qs[5, , t] <- quantile(pars[, 2], c(.05, .5, .95))
     qs[6, , t] <- quantile(pars[, 1], c(.05, .5, .95))
     qs[7, , t] <- quantile(pars[, 3], c(.05, .5, .95))
-    qs[8, , t] <- quantile(sqrt(sig2), c(.05, .5, .95))
-    qs[9, , t] <- quantile(sqrt(tau2), c(.05, .5, .95))
-    qs[10, , t] <- quantile(x, c(.05, .5, .95))
+    qs[8, , t] <- quantile(sqrt(sig_g2), c(.05, .5, .95))
+    qs[9, , t] <- quantile(sqrt(sig_y2), c(.05, .5, .95))
+    qs[10, , t] <- quantile(g_t, c(.05, .5, .95))
   }
   return(list(qs = qs, loglike = loglike, pred = pred))
 }
