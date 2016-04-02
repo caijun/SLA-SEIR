778cae9063df97eac709243c121f30dadffec5e4
caijun <tony.tsai.2046@gmail.com>
2016-04-02 19:32:19 +0800

    add 2008/09 season

--- forecast/server.R
+++ forecast/server.R
@@ -5,7 +5,15 @@
 # # Data set
 # # --------
 # data <- read.table("Google-CDC-US-Feb2010.txt", header = TRUE)
-# y <- data[1:36, 5] # y is ILI percentage (%) of 2003-04 season
+# dateLabel <- format(as.Date(data$DateLabel), format = "%m/%d/%y")
+# # from <- 1 # y is ILI percentage (%) of 2003/2004 season
+# # to <- 36
+# from <- 245 # y is ILI percentage (%) of 2008/2009 season
+# to <- 314
+# y <- data[from:to, 5]
+# tickInd <- trunc(seq(from, to, length = 4))
+# tickLab <- dateLabel[tickInd]
+# tickInd <- tickInd - (from - 1)
 # 
 # # Prior hyperparameters
 # # ---------------------
@@ -27,27 +35,39 @@
 # set.seed(1321654)
 # n <- length(y)
 # yI <- diff(y) / y[1:(n - 1)] # the observed growth rate of the infectious population
-# pf <- PF1(y, yI, a0, b0, c0, d0, ma, sda, mb, sdb, mg, sdg, M, a)
+# pf <- PF(y, yI, a0, b0, c0, d0, ma, sda, mb, sdb, mg, sdg, M, a, ahead = 1)
 # pred.pf <- pf$pred
 # mpred.pf <- t(apply(pred.pf, 2, quantile, c(.05, .5, .95)))
-# save(y, mpred.pf, file = "forecast/draw.RData")
-
-load("draw.RData")
-
-tickind1 <- trunc(seq(1, 36, length = 4))
-ticklab1 <- c("9/28/03", "12/14/03", "3/7/04", "5/30/04")
+# # save(y, mpred.pf, tickInd, tickLab, file = "forecast/draw2003-04.RData")
+# save(y, mpred.pf, tickInd, tickLab, file = "forecast/draw2008-09.RData")
 
 shinyServer(function(input, output) {
+  output$selectInput <- renderUI({
+    selectInput("sel.season", label = "Season", 
+                choices = list("2003/2004", "2008/2009"), 
+                selected = "2003-04")
+  })
+  
+  output$sliderInput <- renderUI({
+    if (input$sel.season == "2003/2004") maxval <- 36 else maxval <- 70
+    
+    sliderInput("slider", label = "Week number",
+                min = 1, max = maxval, value = 1,
+                animate = TRUE, step = 1)
+  })
+  
   output$plot <- renderPlot({
+    if (input$sel.season == "2003/2004") load("draw2003-04.RData") else load("draw2008-09.RData")
+    
     i <- input$slider
-    y <- c(y[1:i], rep(NA, 36 - i))
+    y <- c(y[1:i], rep(NA, length(y) - i))
     mpred.pf <- rbind(mpred.pf, rep(NA, 3))
     plot(y, ylim = c(-1, 20), pch = 15, yaxs = "i", xaxs = "i", xaxt = "n",
          xlab = "", ylab = "", axes = T)
     points(y, pch = 16)
     title(xlab = "", ylab = "Infected %", main = paste0("SEIR Model ", input$sel.season), 
           cex.lab = 1.4)
-    axis(1, at = tickind1, lab = ticklab1)
+    axis(1, at = tickInd, lab = tickLab)
     points(i + 1, 100 * mpred.pf[i, 2], pch = 16, col = "red") # prediction for i + 1 week
     if (i > 1) {
       lines(2:i, 100 * mpred.pf[1:(i - 1), 1], col = grey(0.65), lwd = 2, lty = 2)
--- forecast/ui.R
+++ forecast/ui.R
@@ -1,18 +1,12 @@
 shinyUI(fluidPage(
-  titlePanel("Forecasts by state space SEIR model"),
+  titlePanel("1-week-ahead forecasts by state space model"),
   
   sidebarLayout(
     sidebarPanel(h3("Parameters"), 
-                 selectInput("sel.season", label = "Season", 
-                             choices = list("2003/2004"), 
-                             selected = "2003-04"),
-                 selectInput("sel.step", label = "Steps ahead", 
-                             choices = list("1" = 1), 
-                             selected = 1),
-                 sliderInput("slider", label = "Week number", 
-                             min = 1, max = 36, value = 1, 
-                             animate = TRUE, step = 1),
+                 uiOutput("selectInput"),
+                 uiOutput("sliderInput"),
                  width = 3),
+    
     mainPanel(plotOutput("plot"), width = 9)
   )
 ))
